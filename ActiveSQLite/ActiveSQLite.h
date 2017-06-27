61794953900dccc5fffac5823bae10a41bdf4af1
zhoukai <zhoukai@kaochong.com>
2017-06-27 17:00:16 +0800

    Initial Commit

---
+++ ActiveSQLite/ActiveSQLite.h
@@ -0,0 +1,19 @@
+//
+//  ActiveSQLite.h
+//  ActiveSQLite
+//
+//  Created by yuhan on 27/06/2017.
+//  Copyright © 2017 hereigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActiveSQLite.
+FOUNDATION_EXPORT double ActiveSQLiteVersionNumber;
+
+//! Project version string for ActiveSQLite.
+FOUNDATION_EXPORT const unsigned char ActiveSQLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActiveSQLite/PublicHeader.h>
+
+
